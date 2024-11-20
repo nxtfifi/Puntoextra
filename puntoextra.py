f557176cc0390559ed288dc024f9f89e271acb0c
Luis Antonio López Abaunza <194427-3@iberoleon.edu.mx>
2024-11-20 11:50:18 -0600

    merge
    puse las funciones de los archivos

--- puntoextra.py
+++ puntoextra.py
@@ -1,1 +1,9 @@
 print("Felix")
+print("Toño")
+
+def contarPalabraClave(lista, clave):
+    return lista.count(clave)
+def promedio (lista):
+    if len(lista)==0:
+        return 0
+    return sum(lista)/len(lista)
